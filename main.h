c96eeb0e4cdf7b6438b2403ba7d26ddf8d0da196
Pw-Labuschange <Pw.labuschagne@yahoo.com>
2022-08-11 15:52:29 -0400

    Simple shell attempt #2

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MAXINPUT 1024
+
+extern char **environ;
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int _getline(void);
+int main(int argc, char **argv, char **envp);
+int cmd_builtins(char *str);
+char **parse_line(char *str_in);
+void exec_cmd(char **args, char **env);
+void cd_something(char **args);
+void clear_me(void);
+void exit_me(char **args);
+int cmd_compare(char **args);
+
+
+#endif
